98a7292d151fa474a9ed54d0bf485a7ee17d0f77
tanvirgeekios <tanvir.alam@iosventure.com>
2021-03-28 07:53:41 +0600

    Initial Commit

---
+++ SettingsByMyself/SettingsByMyselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsByMyselfApp.swift
+//  SettingsByMyself
+//
+//  Created by MD Tanvir Alam on 28/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SettingsByMyselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
